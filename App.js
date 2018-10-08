cdcf2f9561dc8549d07749d7242f1a025d799fe1
arisa-hirata <32582917+arisa-hirata@users.noreply.github.com>
2018-10-08 10:19:22 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
-// import Poll from './src/./componentsPoll';
-import LoginForm from './src/components/LogIn/LoginForm';
+import Navigator from './src/components/Navigator';
 
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <ScrollView>
-        <LoginForm />
-
-        {/* <Poll /> */}
-
-      </ScrollView>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-
-});
+export default Navigator;
