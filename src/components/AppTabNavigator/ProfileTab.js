7656f56797b135d487a68b0afd3f8ade43f387bc
Angus Tsai <angusshuntsai@gmail.com>
2018-10-29 21:49:22 -0700

    Profile Page

--- src/components/AppTabNavigator/ProfileTab.js
+++ src/components/AppTabNavigator/ProfileTab.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Dimensions, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, Button, TouchableOpacity } from 'react-native';
 
 import ProgressBarAnimated from 'react-native-progress-bar-animated';
 
 export default class ProfileTab extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
+  state={
       userId: "Katie Munro",
       userLocation: 'Vancouver, British Columbia',
       userFriend: 164,
       totalVote: 100,
       totalWin: 60,
       progress: 20,
-
-    };
-  }
-
-  increase = (key, value) => {
-    this.setState({
-      [key]: this.state[key] + value,
-    });
   }
 
 // take total of votes divided by the total win or lost to get the result
@@ -33,32 +23,87 @@ export default class ProfileTab extends React.Component {
  
     return (
       <View style={styles.container}>
-      <View>
-      <Image 
-          style={{width: 200, height: 200}}
-          source={require('../../imgs/userImg.png')}
-          resizeMode="contain"
-          />
-      </View>
+       
+        <View style={styles.top}>
+        <View style={styles.topBar}>
 
-      <View>
-          <Text>{this.state.userId}</Text>
-          <Text>{this.state.userLocation}</Text>
-          <Text>{this.state.userFriend}</Text>
-          <Text>Poll Mates</Text>  
-      </View>
-
-
-          <Text>Percentage of Total Wins</Text>
-          <ProgressBarAnimated
-            width={barWidth}
-            value={this.state.progress}
-            backgroundColorOnComplete="#6CC644"
-          />
-
-          <View style={styles.pollcontainer}>
-          <Text>My Polls</Text>
+          <View style={styles.topBarItem}>
+            <View style={styles.topBarItemInner}>
+              <Text style={{paddingLeft: 25}}>Archive</Text>
+            </View>
           </View>
+
+            <View style={styles.topBarItem}>
+              <View style={styles.topBarItemInner}>
+                <Text style={{textAlign: 'right', paddingRight: 25}}>Edit</Text>
+              </View>
+            </View>
+          </View>
+{/* **************************************************************************************** */}
+          <View style={styles.ProfileImage}>
+            <Image 
+            style={{width: 230, height: 230}}
+            source={require('../../imgs/userImg.png')}
+            resizeMode = 'contain'
+            />
+          </View>
+        </View>
+{/* **************************************************************************************** */}
+        <View style={styles.nameInput}>
+            <Text style={{fontSize: 25}}>{this.state.userId}</Text>
+        </View>
+
+        <View style={styles.locationInput}>
+            <Text style={{fontSize: 16}}>{this.state.userLocation}</Text>
+        </View>
+
+        <View style={styles.userFriend}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>{this.state.userFriend}</Text>
+        </View>
+{/* **************************************************************************************** */}
+         <View style={styles.pollBar}>
+          <View style={styles.pollBarItem}>
+            <View style={styles.pollBarItemInner}>
+              <Text style={{}}>Polls</Text>
+                </View>
+                  </View>
+
+          <View style={styles.pollBarItem}>
+            <View style={styles.pollBarItemInner}>
+              <Text style={{textAlign: 'right'}}>+</Text>
+                </View>
+                  </View>
+                    </View>
+{/* **************************************************************************************** */}
+        <View style={styles.currentPoll}>
+          <View style={styles.currentPollItem}>
+            <View style={styles.currentPollItemInner}>
+              <Image 
+                style={{width: 130, height: 130}}
+                source={require('../../imgs/userImg.png')}
+                 resizeMode = 'contain'
+              />
+            </View>
+              <View style={styles.currentPollItemInner}>
+              <Image 
+            style={{width: 130, height: 130}}
+            source={require('../../imgs/userImg.png')}
+            resizeMode = 'contain'
+            />
+              </View>
+              </View>
+
+          <View style={styles.currentPollItem}>
+            <View style={styles.currentPollItemInner}>
+            </View>
+          </View>
+
+           <View style={styles.currentPollItem}>
+            <View style={styles.currentPollItemInner}>
+            </View>
+          </View>
+        </View>
+
 
 
       </View>
@@ -69,17 +114,88 @@ export default class ProfileTab extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    backgroundColor: '#FFFF'
+  },
+
+  top: {
+    height: '40%',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    marginTop: 15,
-  },
-  pollcontainer: {
-    flex: 1,
+  ProfileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: 'hidden',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+  },
+  nameInput: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+  locationInput: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+  userFriend: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+
+// ********************************************************************
+    topBar:{
+      height: '10%',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    topBarItem: {
+      width: '50%',
+      padding: 5,
+    },
+    topBarItemInner: {
+      flex: 1,
+    },
+// ********************************************************************
+    pollBar:{
+      height: '5%',
+      width: '75%',
+      flexDirection: 'row',
+      borderBottomWidth: 1
+    },
+    pollBarItem: {
+      width: '50%',
+      padding: 5,
+    },
+    pollBarItemInner: {
+      flex: 1,
+    },
+// ********************************************************************
+    currentPoll: {
+      height: '45%',
+      backgroundColor: '#fff',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      padding: 5,
+    },
+    currentPollItem: {
+      width: '30%',
+      height:'40%',
+      flexDirection: 'row',
+      padding: 5,
+    },
+    currentPollItemInner: {
+      flex: 1,
+      backgroundColor: '#292929',
+      overflow: 'hidden',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
+    
+
+
+
 
 });
+
