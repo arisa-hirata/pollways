2b462e5fa877ccbc27b4e4d75baca43741f7227a
Angus Tsai <angusshuntsai@gmail.com>
2018-11-27 11:42:23 -0800

    Profile page

--- src/components/AppTabNavigator/ProfileTab.js
+++ src/components/AppTabNavigator/ProfileTab.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Dimensions, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions, Button, TouchableOpacity, ScrollView, ImageBackground } from 'react-native';
 //AIzaSyDOzIQCN_wh25kKX-FywqgFcrTay_O2ohk
 //https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyDOzIQCN_wh25kKX-FywqgFcrTay_O2ohk
 //$ npm install react-native-progress --save
 // import ProgressBarAnimated from 'react-native-progress-bar-animated';
-// import Geolocation from 'react-native-geolocation-service';
+import Geolocation from 'react-native-geolocation-service';
 import { connect } from 'react-redux';
 import ImagePicker from 'react-native-image-crop-picker';
 import RNFetchBlob from 'react-native-fetch-blob';
+import { getFB } from "../firebase";
+import { ChangePollID, ChangeIndex } from '../../actions/PollActions';
 
+var firebase = getFB();
 
 const Blob = RNFetchBlob.polyfill.Blob;
 const fs = RNFetchBlob.fs;
@@ -25,7 +28,8 @@ class ProfileTab extends React.Component {
     position: {},
     error: "",
     img: {},
-    // filename: "",
+    filename: "",
+    polls: []
   }
   getPlace = async (lat, long) => {
     console.log("latlng", lat, long);
@@ -41,46 +45,35 @@ class ProfileTab extends React.Component {
       //join will add whatever you input in the join(" ")
       ShowPlace: cityTemp.join(" ")
     });
-    // console.log(cityTemp)
+    console.log("HelloB", cityTemp)
   }
-
+// ComponentDiDmount will automatically run 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.getPlace(
-          position.coords.latitude,
-          position.coords.longitude
-        )
+    this.getPolls()
+    setInterval(() => {
+      if (this.watchID) {
+        return false;
+      }
+
+      this.watchID = Geolocation.getCurrentPosition((position) => {
+        // var initialRegion = {
+        //   latitude:position.coords.latitude,
+        //   longitude:position.coords.longitude,
+        // }
+        this.getPlace(position.coords.latitude, position.coords.longitude)
       },
-    );
+        (error) => {
+          clearInterval(this.watchID)
+          this.watchID = null;
+          console.log(error);
+          this.setState({ error: error.message })
+        },
+        { enableHighAccuracy: true, timeout: 5000, maximumAge: 1000 },
+      );
+    }, 1000)
+    //source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
+
   }
-
-  // componentDidMount() {
-
-  //   setInterval(() => {
-  //     if (this.watchID) {
-  //       return false;
-  //     }
-
-  //     this.watchID = Geolocation.getCurrentPosition((position) => {
-  //       // var initialRegion = {
-  //       //   latitude:position.coords.latitude,
-  //       //   longitude:position.coords.longitude,
-  //       // }
-  //       this.getPlace(position.coords.latitude, position.coords.longitude)
-  //     },
-  //       (error) => {
-  //         clearInterval(this.watchID)
-  //         this.watchID = null;
-  //         console.log(error);
-  //         this.setState({ error: error.message })
-  //       },
-  //       { enableHighAccuracy: true, timeout: 5000, maximumAge: 1000 },
-  //     );
-  //   }, 1000)
-  //   //source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
-
-  // }
   AddImg = async () => {
     const image = await ImagePicker.openPicker({
       width: 160,
@@ -97,7 +90,7 @@ class ProfileTab extends React.Component {
       img: image,
       // filename: image.filename
     });
-    // console.log("whatthefucj", filename)
+    console.log("whatthefucj", filename)
 
     uploadImage = async (refId, direction) => {
       if (!this.state[`img${direction}`] === {}) return undefined
@@ -108,7 +101,71 @@ class ProfileTab extends React.Component {
     }
   };
 
+  getPolls = async () => {
+  // Show only for the user 
+  console.log("HelloAA", this.props.user.user.uid);
+    var polls =  await firebase.firestore().collection("polls").where("uerid", "==", this.props.user.user.uid);
+    console.log("HelloAA", polls)
+    var allPolls = [];
+    console.log("HelloAA", "SnapBefore")
+   var snap = await polls.get();
+   console.log("HelloAA",snap);
+      snap.forEach((doc) => {
+        console.log("HelloAA", doc.data());
+        var obj = doc.data();
+        obj.pollid = doc.id
+        allPolls.push(obj);
+      })
+      this.setState({
+        polls: allPolls
+      })
+
+  }
+
+  seePoll = () => {
+    //Go to this polls Insights
+    this.props.dispatch(ChangePollID(this.allPolls[this.curIndex].doc_id));//dispatch action to change pollid
+    this.props.dispatch(ChangeIndex(this.curIndex));
+    this.setState({
+      title: this.allPolls[this.curIndex].title,
+      rimg: this.allPolls[this.curIndex].options.right.img,
+      limg: this.allPolls[this.curIndex].options.left.img,
+      username: this.allPolls[this.curIndex].username
+    });
+  }
   render() {
+
+    var pollImages = this.state.polls.map((obj,index)=>{
+      return (
+        <TouchableOpacity>
+        <View style={styles.boxContainer}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.textContainer}>{obj.title}</Text>
+            </View>
+            <ImageBackground
+              source={require('../../imgs/1x/Asset14.png')}
+              style={[styles.imgouterContainer, {}]}
+              >
+              <View style={[styles.imgContainer, {borderBottomLeftRadius:10,
+              borderTopLeftRadius:10}]}>
+              <Image
+              style={{width: 440, height: 440, resizeMode:"contain", }}
+              source={{uri: obj.options.right.img }}>
+              </Image>
+              </View>
+  
+              <View style={[styles.imgContainer, {borderBottomRightRadius:10,
+              borderTopRightRadius:10},]}>
+              <Image
+              style={{width: 440, height: 440, resizeMode:"contain", }}
+              source={{uri: obj.options.left.img }}>
+              </Image>
+              </View>
+              </ImageBackground>
+        </View>
+        </TouchableOpacity>
+      )
+    })
 
     console.log(this.props.navigation);
 
@@ -122,13 +179,13 @@ class ProfileTab extends React.Component {
             <View style={styles.topBarItem}>
               <View style={styles.topBarItemInner}>
                 <View style={{ paddingLeft: 25, marginTop: 20 }}>
-                  {/* <TouchableOpacity>
+                  <TouchableOpacity onPress={() => this.props.navigation.navigate('Tutorials')}>
                     <Image
                       style={{ width: 20, height: 20 }}
                       source={require('../../imgs/Archive.png')}
                       resizeMode="contain"
                     />
-                  </TouchableOpacity> */}
+                  </TouchableOpacity>
                 </View>
               </View>
             </View>
@@ -166,23 +223,7 @@ class ProfileTab extends React.Component {
           <View style={styles.nameInput}>
             <Text style={{ fontSize: 25 }}>{this.state.ShowPlace}</Text>
           </View>
-          {/* user friend number**************************************************************************************** */}
-          {/* <View style={styles.userFriend}>
-            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{this.state.userFriend}</Text>
-          </View> */}
 
-          {/* <View style={styles.userFriend}>
-            <Text style={{ fontSize: 15 }}>Poll Mates Hello</Text>
-          </View> */}
-
-          {/* **************************************************************************************** */}
-
-          {/* <View style={{ width: '100%', height: '10%', backgroundColor: '#E2FFE8', justifyContent: 'center' }}>
-            <Text>Winning Bar </Text>
-            <Progress.Bar progress={0.8} width={200} />
-          </View> */}
-
-          {/* **************************************************************************************** */}
           <View style={styles.pollBar}>
             <View style={styles.pollBarItem}>
               <View style={styles.pollBarItemInner}>
@@ -204,83 +245,12 @@ class ProfileTab extends React.Component {
             </View>
           </View>
           {/* **************************************************************************************** */}
-          <View style={styles.currentPoll}>
-            <View style={styles.currentPollItem}>
-              <View style={styles.currentPollItemInner}>
-                <Image
-                  style={{ width: 200, height: 200 }}
-                  source={require('../../imgs/donut.jpg')}
-                  resizeMode='contain'
-                />
-              </View>
-              <View style={styles.currentPollItemInner}>
-                <Image
-                  style={{ width: 200, height: 200 }}
-                  source={require('../../imgs/cupcake.jpg')}
-                  resizeMode='contain'
-                />
-              </View>
-            </View>
-
-            <View style={styles.currentPollItem}>
-              <View style={styles.currentPollItemInner}>
-              </View>
-            </View>
-
-            <View style={styles.currentPollItem}>
-              <View style={styles.currentPollItemInner}>
-              </View>
-            </View>
+          <View style={styles.containerboxPoll}>
+              {pollImages}
           </View>
-          {/* **************************************************************************************** */}
-          {/* <View style={styles.pollBar}>
-            <View style={styles.pollBarItem}>
-              <View style={styles.pollBarItemInner}>
-                <Text style={{}}>Group polls</Text>
-              </View>
-            </View>
-            <View style={styles.pollBarItem}>
-              <View style={[styles.pollBarItemInner, { alignItems: 'flex-end' }]}>
-                <View style={{ width: 20, height: 20, justifyContent: 'center', alignItems: 'center' }}>
-                  <TouchableOpacity>
-                    <Image
-                      style={{ width: 10, height: 10 }}
-                      source={require('../../imgs/add1.png')}
-                    />
-                  </TouchableOpacity>
-                </View>
-              </View>
-            </View>
-          </View> */}
-          {/* **************************************************************************************** */}
-          {/* <View style={styles.groupPoll}>
-            <View style={styles.groupPollItem}>
-              <View style={styles.groupPollItemInner}>
-                <Image
-                  style={{ width: 200, height: 200 }}
-                  source={require('../../imgs/donut.jpg')}
-                  resizeMode='contain'
-                />
-              </View>
-              <View style={styles.groupPollItemInner}>
-                <Image
-                  style={{ width: 200, height: 200 }}
-                  source={require('../../imgs/cupcake.jpg')}
-                  resizeMode='contain'
-                />
-              </View>
-            </View>
-            <View style={styles.groupPollItem}>
-              <View style={styles.groupPollItemInner}>
-              </View>
-            </View>
-            <View style={styles.groupPollItem}>
-              <View style={styles.groupPollItemInner}>
-              </View>
-            </View>
-          </View> */}
+       
         </View>
-      </ScrollView>
+        </ScrollView>
     );
   }
 }
@@ -351,55 +321,45 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   // ********************************************************************
-  currentPoll: {
-    height: '15%',
-    width: '100%',
+  boxContainer: {
+    width: 120,
+    height: 160,
+    margin: 5,
+    marginBottom: 50,
+  },
+  titleContainer: {
+    backgroundColor:"#F1E29E",
+    alignItems: "center",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  textContainer: {
+    padding: 5,
+    margin: 5,
+  },
+  imgouterContainer: {
+    width: "100%",
+    height: "100%",
+    flexDirection: "row",
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 5,
-    marginBottom: 10,
-    overflow: 'hidden',
   },
-  currentPollItem: {
-    width: '30%',
-    height: '100%',
-    flexDirection: 'row',
-    padding: 5,
-    overflow: 'hidden',
+  imgContainer: {
+    alignItems: 'center',
+    width: "40%",
+    height: "80%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: "hidden",
   },
-  currentPollItemInner: {
+  containerboxPoll: {
     flex: 1,
-    backgroundColor: '#292929',
-    overflow: 'hidden',
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden',
-  },
-  // ********************************************************************
-  groupPoll: {
-    height: '15%',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 5,
-    backgroundColor: '#FDF2E9'
-  },
-  groupPollItem: {
-    width: '30%',
-    height: '100%',
-    flexDirection: 'row',
-    padding: 5,
-  },
-  groupPollItemInner: {
-    flex: 1,
-    backgroundColor: '#292929',
-    overflow: 'hidden',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width:"100%",
+    flexDirection:"row",
+    alignItems: "flex-start",
+    flexWrap: "wrap",
+    paddingLeft:10
+
   }
 });
 
